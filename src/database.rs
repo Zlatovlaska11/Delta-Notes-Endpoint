87506db42776d5305de60e3b092e4e0066a60585
Zlatovlaska11 <marpecqueur@gmail.com>
2024-05-03 18:52:34 +0200

    removed create table because it wont deploy :(

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,6 @@
 pub mod db_work {
 
-    use async_std::println;
-    use tokio_postgres::{row, Client, Error, GenericClient, NoTls};
+    use tokio_postgres::{Client, Error, GenericClient, NoTls};
 
     pub async fn get_connection(conn_str: String) -> Client {
         // Create a connection string
@@ -49,17 +48,5 @@ pub mod db_work {
                 column1, column2, column3
             );
         }
-    }
-
-    pub async fn create_table(table_name: String, conn_str: String) {
-        let client = get_connection(conn_str).await;
-
-        let rows = client
-            .query(
-                "CREATE TABLE users ( username: text, password: text, mail: text) ",
-                &[],
-            )
-            .await
-            .unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ pub mod enpoint;
 
 #[shuttle_runtime::main]
 async fn tide(#[shuttle_shared_db::Postgres] conn_str: String) -> shuttle_tide::ShuttleTide<()> {
-    database::db_work::create_table("users".to_string(), conn_str.clone()).await;
     enpoint::server::start_server(conn_str).await
 
 }
