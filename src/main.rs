5ffa87d759edbfd357027bd5d732ed7abdaf610c
Zlatovlaska11 <marpecqueur@gmail.com>
2024-05-13 11:00:53 +0200

    some small updates and a file bucket implementation

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ use async_std::println;
 
 pub mod auth;
 pub mod database;
-
+pub mod filebucket;
 pub mod enpoint;
 
 #[shuttle_runtime::main]
 async fn tide(#[shuttle_shared_db::Postgres] conn_str: String) -> shuttle_tide::ShuttleTide<()> {
-enpoint::server::start_server(conn_str).await
-
+    enpoint::server::start_server(conn_str).await
 }
