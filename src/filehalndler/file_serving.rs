8c32dcd00fc06aece48fbd68c4bb875dce07702c
Zlatovlaska11 <marpecqueur@gmail.com>
2024-06-01 13:45:23 +0200

    feat: working with dokerized file storage, mayebe will improve later, we'll see

--- src/filehalndler/file_serving.rs
+++ src/filehalndler/file_serving.rs
@@ -31,14 +31,14 @@ pub mod file_serve {
     pub async fn pptx_viewer(req: Request<()>) -> tide::Result {
         let params: PptxParams = req.query()?;
         let path = get_filepath(params.filename, params.course_id)?.replace(" ", "%20");
-        print!("{}", path);
-        let path = encode(path.as_ref());
+        print!("{}", &path[6..path.len()]);
+        let path = encode(path[6..path.len()].as_ref());
 
         //NEEDS TO BE DELETED
-        let host = "https://zlatovlas-delta-notes.shuttleapp.rs".to_string();
+        let host = "pup-settled-dolphin.ngrok-free.app".to_string();
         let office_viewer_url = format!(
             "https://view.officeapps.live.com/op/view.aspx?src={}",
-            (&format!("{}/fls/{}", host, path))
+            (&format!("{}/files/{}", host, path.replace("/public/", "/files/")))
         );
         print!("{}", office_viewer_url);
         Ok(Redirect::new(office_viewer_url).into())
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub mod enpoint;
 pub mod filehalndler;
 
 #[shuttle_runtime::main]
-async fn tide(#[shuttle_shared_db::Postgres] conn_str: String) -> shuttle_tide::ShuttleTide<()> {
+async fn tide(/*#[shuttle_shared_db::Postgres] conn_str: String*/) -> shuttle_tide::ShuttleTide<()> {
+    let conn_str = "sothuesnatuhnsa".to_string();
     enpoint::server::start_server(conn_str).await
 }
