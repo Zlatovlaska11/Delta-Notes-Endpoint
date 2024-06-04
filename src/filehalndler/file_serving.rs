3451569a3aee5af0d123484d1f758a638a20f20a
Zlatovlaska11 <marpecqueur@gmail.com>
2024-06-04 15:04:03 +0200

    some small changes in the architecture and shit...

--- src/filehalndler/file_serving.rs
+++ src/filehalndler/file_serving.rs
@@ -9,22 +9,20 @@ pub mod file_serve {
     #[derive(Debug, Deserialize)]
     struct PptxParams {
         filename: String,
-        course_id: u8,
+        id: u8,
     }
-
 
     pub async fn pptx_viewer(req: Request<()>) -> tide::Result {
         let params: PptxParams = req.query()?;
-        let path = get_filepath(params.filename, params.course_id)?.replace(" ", "%20");
+        let path = get_filepath(params.filename, params.id)?;
         print!("{}", &path[6..path.len()]);
         let path = encode(path[6..path.len()].as_ref());
 
         let host = "pup-settled-dolphin.ngrok-free.app".to_string();
-        let office_viewer_url = format!(
-            "https://view.officeapps.live.com/op/view.aspx?src={}",
-            (&format!("{}/files/{}", host, path.replace("/public/", "/files/")))
-        );
+        let office_viewer_url = format!("{}/files/{}", host, path.replace("/public/", "/files/"));
         print!("{}", office_viewer_url);
-        Ok(Redirect::new(office_viewer_url).into())
+        let mut resp = Response::new(200);
+        resp.set_body(office_viewer_url);
+        Ok(resp.into())
     }
 }
